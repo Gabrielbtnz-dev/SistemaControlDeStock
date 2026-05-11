c9e29da4a3283728f5a615fee2a9a9cce28899b5
Gabriel Benitez Duarte <165432952+Gabrielbtnz-dev@users.noreply.github.com>
2026-05-11 19:52:33 -0300

    Se agrega entidad para ventas

---
+++ SistemaFB/src/main/java/com/sistema/sistema/Domain/sales.java
@@ -0,0 +1,85 @@
+package com.sistema.sistema.Domain;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "ventas")
+public class sales {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "id_person")
+    private Person person;
+    @Column(name = "valor_total")
+    private BigDecimal valorTotal;
+    @Column(name = "valor_regularizado")
+    private BigDecimal valorRegularizado;
+    @Column(name = "valor_pendiente")
+    private BigDecimal valorPendiente;
+    private String observaciones;
+
+    public sales(){
+
+    }
+
+    public sales( Person person, BigDecimal valorTotal, BigDecimal valorRegularizado, BigDecimal valorPendiente, String observaciones){
+        this.person = person;
+        this.valorTotal = valorTotal;
+        this.valorPendiente = valorPendiente;
+        this.valorRegularizado = valorRegularizado;
+        this.observaciones = observaciones;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public BigDecimal getValorPendiente() {
+        return valorPendiente;
+    }
+
+    public void setValorPendiente(BigDecimal valorPendiente) {
+        this.valorPendiente = valorPendiente;
+    }
+
+    public BigDecimal getValorRegularizado() {
+        return valorRegularizado;
+    }
+
+    public void setValorRegularizado(BigDecimal valorRegularizado) {
+        this.valorRegularizado = valorRegularizado;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+}
